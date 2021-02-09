de3b48a690c2b1d62433f9f302185cd1e73fd1a4
Sytar Nikolay <sitarnet@ukr.net>
2021-02-09 17:04:09 +0200

    Lesson 9. Task 4

--- js/lesson_9.js
+++ js/lesson_9.js
@@ -463,54 +463,134 @@
 // Каждый элемент <th> имеет атрибут data-type:
 
 
-grid.onclick = function(e) {
-    if (e.target.tagName != 'TH') return;
+// grid.onclick = function(e) {
+//     if (e.target.tagName != 'TH') return;
+//
+//     let th = e.target; // цель
+//     // если ячейка TH, тогда сортировать
+//     // cellIndex - это номер ячейки th:
+//     //   0 для первого столбца
+//     //   1 для второго и т.д.
+//     sortGrid(th.cellIndex, th.dataset.type); // номер ячейки в строке, data-type = number или string
+// };
+//
+// function sortGrid(colNum, type) {
+//     let tbody = grid.querySelector('tbody'); // озвращает первый элемент (Element) документа,
+//     // который соответствует указанному селектору или группе селекторов.
+//
+//     let rowsArray = Array.from(tbody.rows); // создаём массив из коллекции строк tr в tbody
+//
+//     // compare(a, b) сравнивает две строки, нужен для сортировки
+//     let compare;
+//
+//     switch (type) {
+//         case 'number': // если число
+//             compare = function(rowA, rowB) {
+//                 return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML; // возвращаем разницу
+//                 // значения (HTML разметка дочерного элемента) ячейки в переменной rowA и
+//                 // значения (HTML разметка дочерного элемента) ячейки в переменной rowB
+//             };
+//             break;
+//         case 'string': // если строка
+//             compare = function(rowA, rowB) {
+//                 return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1; // если
+//                 // значения (HTML разметка дочерного элемента) ячейки в переменной rowA больше
+//                 // значения (HTML разметка дочерного элемента) ячейки в переменной rowB, то = 1, иначе = -1
+//             };
+//             break;
+//     }
+//
+//     rowsArray.sort(compare); // на месте сортирует элементы массива и возвращает отсортированный массив.
+//
+//     tbody.append(...rowsArray); // добавлет в конец, расширяет массив где несколько аргументов (расскрывает массив)
+// }
 
-    let th = e.target; // цель
-    // если ячейка TH, тогда сортировать
-    // cellIndex - это номер ячейки th:
-    //   0 для первого столбца
-    //   1 для второго и т.д.
-    sortGrid(th.cellIndex, th.dataset.type); // номер ячейки в строке, data-type = number или string
-};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function sortGrid(colNum, type) {
-    let tbody = grid.querySelector('tbody'); // озвращает первый элемент (Element) документа,
-    // который соответствует указанному селектору или группе селекторов.
+// Task 4
 
-    let rowsArray = Array.from(tbody.rows); // создаём массив из коллекции строк tr в tbody
+// Поведение "подсказка"
 
-    // compare(a, b) сравнивает две строки, нужен для сортировки
-    let compare;
+// Напишите JS-код, реализующий поведение «подсказка».
 
-    switch (type) {
-        case 'number': // если число
-            compare = function(rowA, rowB) {
-                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML; // возвращаем разницу
-                // значения (HTML разметка дочерного элемента) ячейки в переменной rowA и
-                // значения (HTML разметка дочерного элемента) ячейки в переменной rowB
-            };
-            break;
-        case 'string': // если строка
-            compare = function(rowA, rowB) {
-                return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1; // если
-                // значения (HTML разметка дочерного элемента) ячейки в переменной rowA больше
-                // значения (HTML разметка дочерного элемента) ячейки в переменной rowB, то = 1, иначе = -1
-            };
-            break;
+// При наведении мыши на элемент с атрибутом data-tooltip, над ним должна показываться подсказка и скрываться
+// при переходе на другой элемент.
+
+// Пример HTML с подсказками:
+
+// <button data-tooltip="эта подсказка длиннее, чем элемент">Короткая кнопка</button>
+// <button data-tooltip="HTML<br>подсказка">Ещё кнопка</button>
+
+// В этой задаче мы полагаем, что во всех элементах с атрибутом data-tooltip – только текст. То есть,
+// в них нет вложенных тегов (пока).
+
+// Детали оформления:
+
+// Отступ от подсказки до элемента с data-tooltip должен быть 5px по высоте.
+// Подсказка должна быть, по возможности, посередине элемента.
+// Подсказка не должна вылезать за границы экрана, в том числе если страница частично прокручена, если нельзя
+// показать сверху – показывать снизу элемента.
+// Текст подсказки брать из значения атрибута data-tooltip. Это может быть произвольный HTML.
+// Для решения вам понадобятся два события:
+
+// mouseover срабатывает, когда указатель мыши заходит на элемент.
+// mouseout срабатывает, когда указатель мыши уходит с элемента.
+// Примените делегирование событий: установите оба обработчика на элемент document, чтобы отслеживать «заход»
+// и «уход» курсора на элементы с атрибутом data-tooltip и управлять подсказками с их же помощью.
+
+// После реализации поведения – люди, даже не знакомые с JavaScript смогут добавлять подсказки к элементам.
+
+// P.S. В один момент может быть показана только одна подсказка.
+
+let tooltipElem;
+
+document.onmouseover = function(event) { // вешаем, когда наводишь мышку
+    let target = event.target; // цель
+
+    let tooltipHtml = target.dataset.tooltip;
+    if (!tooltipHtml) return; // если у нас есть подсказка
+
+    // ...создадим элемент для подсказки
+
+    tooltipElem = document.createElement('div'); // создаём элемент
+    tooltipElem.className = 'tooltip'; // создаём класс
+    tooltipElem.innerHTML = tooltipHtml; // вставляем текст подсказки
+    document.body.append(tooltipElem); // добовляем в конец <div class="tooltip">Текст подсказки</div>
+
+    // спозиционируем его сверху от аннотируемого элемента (top-center)
+    let coords = target.getBoundingClientRect(); // возвращает размер элемента и его позицию относительно viewport
+    // (часть страницы, показанная на экране, и которую мы видим).
+
+    let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+
+    // .left - возвращает значение левой координаты DOMRect.
+    // .offsetWidth - возвращает ширину элемента. Как правило, offsetWidth — это значение, включающее горизонтальный отступ элемента,
+    // ширину вертикального скроллбара (если он есть) и CSS ширину.
+
+    if (left < 0) left = 0; // не заезжать за левый край окна
+
+    let top = coords.top - tooltipElem.offsetHeight - 5;
+
+    // .top - возвращает значение верхней координаты DOMRect.
+    // .offsetHeight - высота элемента с учетом вертикальных полей и границ в пикселях.
+    // Свойство неизменяемое, только для чтения.
+
+    if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+        top = coords.top + target.offsetHeight + 5;
     }
 
-    rowsArray.sort(compare); // на месте сортирует элементы массива и возвращает отсортированный массив.
+    tooltipElem.style.left = left + 'px';
+    tooltipElem.style.top = top + 'px';
+};
 
-    tbody.append(...rowsArray); // добавлет в конец, расширяет массив где несколько аргументов (расскрывает массив)
-}
+document.onmouseout = function(e) { // вешаем, когда отводишь мышку
 
+    if (tooltipElem) { // если true
+        tooltipElem.remove(); // удаляет указанные токены из списка.
+        tooltipElem = null; // присваеваем null
+    }
 
-
-
-
-
-
+};
 
 
 
