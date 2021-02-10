ff032e6f9f61da329c40055b1b9ff8f2d0b7d6ff
Sytar Nikolay <sitarnet@ukr.net>
2021-02-10 17:32:25 +0200

    Lesson 10. Task

--- js/lesson_10.js
+++ js/lesson_10.js
@@ -184,3 +184,45 @@
 // В следующей главе мы поговорим о событиях, которые возникают при передвижении мыши, и об отслеживании смены
 // элементов под указателем.
 
+// Task 1
+
+// Выделяемый список
+
+// Создайте список, в котором элементы могут быть выделены, как в файловых менеджерах.
+
+// При клике на элемент списка выделяется только этот элемент (добавляется класс .selected), отменяется выделение
+// остальных элементов.
+
+// Если клик сделан вместе с Ctrl (Cmd для Mac), то выделение переключается на элементе, но остальные элементы при
+// этом не изменяются.
+
+// P.S. В этом задании все элементы списка содержат только текст. Без вложенных тегов.
+// P.P.S. Предотвратите стандартное для браузера выделение текста при кликах.
+
+ul.onclick = function(event) {
+    if (event.target.tagName != "LI") return;
+
+    if (event.ctrlKey || event.metaKey) {
+        toggleSelect(event.target);
+    } else {
+        singleSelect(event.target);
+    }
+
+}
+
+// предотвращает ненужное выделение элементов списка при клике
+ul.onmousedown = function() {
+    return false;
+};
+
+function toggleSelect(li) {
+    li.classList.toggle('selected'); // если есть, то скрывает, если нет, то открывает
+}
+
+function singleSelect(li) {
+    let selected = ul.querySelectorAll('.selected');
+    for(let elem of selected) {
+        elem.classList.remove('selected'); // удаляет
+    }
+    li.classList.add('selected'); // добавляет
+}
